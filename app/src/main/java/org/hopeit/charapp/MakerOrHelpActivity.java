e07c98a3a33034812badcfe7fdb3b45bba43afcf
Veriqus <android@veriqus.com>
2017-10-28 02:18:00 +0200

    scrollviews inside fragments

--- app/src/main/java/org/hopeit/charapp/MakerOrHelpActivity.java
+++ app/src/main/java/org/hopeit/charapp/MakerOrHelpActivity.java
@@ -3,6 +3,8 @@ package org.hopeit.charapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MakerOrHelpActivity extends AppCompatActivity {
 
@@ -10,6 +12,22 @@ public class MakerOrHelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maker_or_help);
+
+        View viewHelp = (RelativeLayout) findViewById(R.id.help_with_money);
+        viewHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toHelpActivity();
+            }
+        });
+
+        View viewMaker = (RelativeLayout) findViewById(R.id.mail_us);
+        viewMaker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toMakerActivity();
+            }
+        });
     }
 
     public void toHelpActivity(){
